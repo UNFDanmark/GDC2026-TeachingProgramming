1527f1dbcb0be8fc381bc9ee4c90eea961165f50
Xander Raaen <XanderRaaen@proton.me>
2026-07-01 20:24:14 +0200

    Ability to shoot, and fixing of shoot-related bugs

---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootingScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public InputAction shootingInput;
+    public float cooldown;
+
+    float cooldownLeft = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        shootingInput.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownLeft -= Time.deltaTime; //Forklar -=
+        if (shootingInput.WasPressedThisFrame() && cooldownLeft <= 0)
+        {
+            cooldownLeft = cooldown;
+            Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+    }
+}
