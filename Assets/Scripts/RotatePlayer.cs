6287f35242bd657cf91a533099a576b9c9afdc69
Xander Raaen <XanderRaaen@proton.me>
2026-07-01 18:52:30 +0200

    implemented  opgave.
    - tilføjede rotation drevet af input

--- Assets/Scripts/RotatePlayer.cs
+++ Assets/Scripts/RotatePlayer.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RotatePlayer : MonoBehaviour
 {
+    
+    public InputAction rotateAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rotateAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 5, 0);
+        if (rotateAction.ReadValue<float>() > 0)
+        {
+            transform.Rotate(0, 5, 0);    
+        }
+        else if (rotateAction.ReadValue<float>() < 0)
+        {
+            transform.Rotate(0, -5, 0);
+        }
+        
+        
+        
     }
 }
