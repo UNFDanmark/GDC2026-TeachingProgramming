b6f5323af75e0684046a48eb98ef2d3e1b1b2702
Xander Raaen <XanderRaaen@proton.me>
2026-07-04 14:43:28 +0200

    tilføjede enemy og enemy script og tilføjde movement

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
 
+    public float sørensSpeed;
     int SørensHealth;
 
-    float Søren2 = 2.17f;
+    Rigidbody rb;
+    public InputAction moveAction;
 
 
     bool ErSørenSur;
@@ -20,7 +23,9 @@ public class PlayerScript : MonoBehaviour
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
+    {
+        moveAction.Enable();
+        rb = GetComponent<Rigidbody>();
         print(SørensNavn);
     }
 
@@ -28,6 +33,25 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector2 inputValue = moveAction.ReadValue<Vector2>();
+        
+        // copy
+        Vector3 newVelocity = rb.linearVelocity;
+        
+        newVelocity.x = inputValue.x * sørensSpeed;
+        newVelocity.z = inputValue.y * sørensSpeed;
+        
+        rb.linearVelocity = newVelocity;
+        
+        
+        if (sørensSpeed > 8)
+        {
+            print("wow du er mega hurtig... søren");
+        }else print("du er sku lidt langsom");
+        
+        
+        
         print("hej Søren");
         print(SørensNavn);
         
