c9bae784f0ca53c2fbfe1d460d1c0c29f8670fc6
Xander Raaen <XanderRaaen@proton.me>
2026-07-04 14:13:19 +0200

    added sørens variabels

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,9 +2,26 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+
+    int SørensHealth;
+
+    float Søren2 = 2.17f;
+
+
+    bool ErSørenSur;
+    // type variabelNavn værdi
+    
+    Vector3 SørensLokation;
+    public GameObject SørensGameObject;
+    Transform SørensTransform;
+    string SørensNavn = "søren";
+    PlayerScript sørensPlayerScript;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    { 
+        print(SørensNavn);
     }
 
     // hewleijfwelkrlsefeljfnwrg
@@ -12,6 +29,7 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         print("hej Søren");
+        print(SørensNavn);
         
     }
 }
