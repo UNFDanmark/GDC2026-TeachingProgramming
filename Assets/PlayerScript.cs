becb8d952ba693d529144c438994e755e7ca6e71
Xander Raaen <XanderRaaen@proton.me>
2026-07-04 13:46:55 +0200

    tilføjede søren og gav rigidbody

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // hewleijfwelkrlsefeljfnwrg
+    // Update is called once per frame
+    void Update()
+    {
+        print("hej Søren");
+        
+    }
+}
